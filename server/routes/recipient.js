9819435bae134c57a4d182166b772198637475ba
Henry Castro <henry.castro87@gmail.com>
2018-07-03 10:00:34 -0400

    add recipient create route

--- server/routes/recipient.js
+++ server/routes/recipient.js
@@ -25,6 +25,7 @@ class Recipient {
 
 				if (req.query.name) {
 					recipients = recipients
+						// TODO: make search more flexible
 						.filter(recipient => (recipient.name === req.query.name))
 				}				
 
@@ -35,7 +36,38 @@ class Recipient {
 			})
 	}
 
-	create (req, res, next) {}
+	create (req, res, next) {
+		// TODO: add in condition for name being passed as query string
+		if (req.body.recipient === undefined) {
+			if (req.body.recipient.name === undefined) {
+				res.json(400, "no recipient provided!")	
+			} else {
+				res.json(400, "no recipient name provided!")
+			}
+		} else {
+			const options = {
+				method: 'POST',
+				uri: `${process.env.COOLPAY_ENDPOINT}/recipients`,
+				headers: {
+					"Authorization": `Bearer ${req.bearerToken}`
+				},
+				body: {
+					"recipient": {
+						"name": req.body.name
+					}
+				},
+				json: true
+			}
+
+			rp(options)
+				.then(body => {
+					res.json(200, 'New recipient created!')
+				})
+				.catch(error => {
+					res.json(error)
+				})
+		}
+	}
 }
 
 module.exports = new Recipient
