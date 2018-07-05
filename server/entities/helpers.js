74d4361050f0979bb0eb16ba50dff3693a37180e
Henry Castro <henry.castro87@gmail.com>
2018-07-05 03:52:23 -0400

    validate payments, abstraction

---
+++ server/entities/helpers.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const CURRENCY = ["AFN", "ALL", "DZD", "EUR", "AOA", "XCD", "ARS", "AMD", "AUD", "AZN", "BSD", "BHD", "BDT", "BBD", "BYR", "BZD", "XOF", "BTN", "BOB", "BAM", "BWP", "BRL", "BND", "BGN", "BIF", "KHR", "XAF", "CAD", "CVE", "CLP", "CNY", "COP", "KMF", "CDF", "CRC", "HRK", "CUP", "CYP", "CZK", "DKK", "DJF", "DOP", "USD", "EGP", "ERN", "EEK", "ETB", "FJD", "GMD", "GEL", "GHC", "GTQ", "GNF", "GYD", "HTG", "HNL", "HUF", "ISK", "INR", "IDR", "IRR", "IQD", "ILS", "JMD", "JPY", "JOD", "KZT", "KES", "KPW", "KRW", "KWD", "KGS", "LAK", "LVL", "LBP", "LSL", "LRD", "LYD", "CHF", "LTL", "MKD", "MGA", "MWK", "MYR", "MVR", "MTL", "MRO", "MUR", "MXN", "MDL", "MNT", "MAD", "MZM", "MMK", "NAD", "NPR", "NZD", "NIO", "NGN", "NOK", "OMR", "PKR", "PAB", "PGK", "PYG", "PEN", "PHP", "PLN", "QAR", "RON", "RUB", "RWF", "WST", "STD", "SAR", "RSD", "SCR", "SLL", "SGD", "SKK", "SBD", "SOS", "ZAR", "LKR", "SDD", "SRD", "SZL", "SEK", "SYP", "TJS", "TZS", "THB", "TOP", "TTD", "TND", "TRY", "TMM", "UGX", "UAH", "AED", "GBP", "UYU", "UZS", "VUV", "VEB", "VND", "YER", "ZMK", "ZWD", "TWD", "XPF", "GGP", "IMP", "JEP", "BMD", "KYD", "FKP", "GIP", "SHP", "HKD", "MOP", "AWG", "ANG"]
+
+module.exports = {
+	CURRENCY: CURRENCY
+}
--- server/routes/payment.js
+++ server/routes/payment.js
@@ -2,17 +2,19 @@
 
 const rp = require('request-promise')
 
+const paymentValidator = require('../validators/payment')
+
 class Payment {
 	register (app, middleware) {
 		app.namespace('/payment', middleware, () => {
 			app.get('/', this.list)
-			app.post('/', this.create)
+			app.post('/', paymentValidator.validatePayment, this.create)
 		})
 	}
 
 	list (req, res, next) {
 		const options = {
-			uri: `${process.env.COOLPAY_ENDPOINT}/payments`,
+			uri: `${process.env.COOLPAY_BASE_URL}/payments`,
 			headers: {
 				"Authorization": `Bearer ${req.bearerToken}`
 			},
@@ -28,7 +30,36 @@ class Payment {
 			})
 	}
 
-	create (req, res, next) {}
+	create (req, res, next) {
+    const payment = req.body.payment
+
+    const options = {
+      method: 'POST',
+      uri: `${process.env.COOLPAY_BASE_URL}/payments`,
+      headers: {
+        "Authorization": `Bearer ${req.bearerToken}`
+      },
+      body: {
+        "payment": {
+          "amount": payment.amount,
+          "currency": payment.currency,
+          "recipient_id": payment.recipient_id
+        }
+      },
+      json: true
+    }
+
+    rp(options)
+      .then(() => {
+        res.json(200, 'Payment submitted!')
+      })
+      .catch(error => {
+        // catch invalid recipients
+        if (error.statusCode === 422) res.json(422, 'Recipient does not exist!')
+
+        else next(error)
+      })
+	}
 }
 
 module.exports = new Payment
