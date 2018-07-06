c2518cce17665e748a009c7c7dde291363664134
Henry Castro <>
2018-07-06 03:08:51 -0400

    add tests for recipient route

--- test/server/mocks/expressMock.js
+++ test/server/mocks/expressMock.js
@@ -1,8 +1,22 @@
 'use strict'
 
-const sinon = require 'sinon'
+const sinon = require('sinon')
 
 let req = {
   body: {},
   query: {}
+}
+
+let res = {
+  json: (status, result) => {}
+}
+sinon.spy(res, 'json')
+
+let next = sinon.spy()
+
+module.exports = {
+  req: req,
+  res: res,
+  next: next,
+  args: [req, res, next]
 }
